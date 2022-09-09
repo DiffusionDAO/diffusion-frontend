e3d08fa96577d44cbaca2cce9b5df05152e0ef76
DiffusionDAO <diffusionDAO@protonmail.com>
2022-09-09 11:06:52 +0800

    update manifest error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,7 +44,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;600&amp;display=swap" rel="stylesheet" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/logo.png" />
-          <link rel="manifest" href="/manifest.json" />
+          {/* <link rel="manifest" href="/manifest.json" /> */}
         </Head>
         <body>
           <noscript>
