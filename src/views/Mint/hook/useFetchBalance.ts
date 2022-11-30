0d7bcbdca6c268ccceaa627d1d91305118c92922
DiffusionDAO <diffusiondao@protonmail.com>
2022-11-30 12:46:55 +0800

    update paginate for NFTMarket

---
+++ src/views/Mint/hook/useFetchBalance.ts
@@ -0,0 +1,30 @@
+import { useTokenContract } from 'hooks/useContract'
+import useSWR from 'swr'
+import { useWeb3React } from '@pancakeswap/wagmi'
+import { getDFSAddress } from 'utils/addressHelpers'
+import { BigNumber } from '@ethersproject/bignumber'
+
+export const useFetchBalance = () => {
+  const address = getDFSAddress()
+  const tokenContract = useTokenContract(address)
+  const { account } = useWeb3React()
+
+  const { data } = useSWR('dfsBalance', async () => {
+    const val = await tokenContract.balanceOf(account)
+    return val
+  })
+
+  return { balance: data || BigNumber.from(0) }
+}
+
+export const useFetchAllowance = (spender) => {
+  const address = getDFSAddress()
+  const tokenContract = useTokenContract(address)
+  const { account } = useWeb3React()
+
+  const { data } = useSWR('dfsAllowance', async () => {
+    const val = await tokenContract.allowance(account, spender)
+    return val
+  })
+  return { allowance: data || BigNumber.from(0) }
+}
