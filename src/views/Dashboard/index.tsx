c482d6f4e11f0817cf2b341f7c7b7c77a9b15c02
DiffusionDAO <diffusiondao@protonmail.com>
2022-12-08 00:02:05 +0800

    update dashboard

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from '@pancakeswap/localization'
 import { makeStyles } from '@material-ui/core/styles'
 import { useState } from 'react'
 import useSWR from 'swr'
-import { useMatchBreakpoints } from '@pancakeswap/uikit'
+import { Spinner, useMatchBreakpoints } from '@pancakeswap/uikit'
 import { useBondContract, useDFSContract, useDFSMiningContract, usePairContract } from 'hooks/useContract'
 import { getDFSAddress, getPairAddress, getUSDTAddress } from 'utils/addressHelpers'
 import { BigNumber } from '@ethersproject/bignumber'
@@ -66,23 +66,7 @@ const Dashboard = () => {
   const { isMobile } = useMatchBreakpoints()
   const classes = useStyles()
   const [activeTab, setActiveTab] = useState<string>('Overview')
-  const [callFactor, setCallfactor] = useState<number>(0)
-  const [tvl, setTvl] = useState<BigNumber>(BigNumber.from(0))
-  const [solitaryReserves, setSingleCurrencyReserves] = useState<number>(0)
-  const [totalCirculation, setTotalCirculation] = useState<BigNumber>(BigNumber.from(0))
-  const [circulationSupply, setCirculationSupply] = useState<BigNumber>(BigNumber.from(0))
-  const [foundationDFS, setFoundationDFS] = useState<BigNumber>(BigNumber.from(0))
-  const [totalBondUsed, setTotalBondUsed] = useState<BigNumber>(BigNumber.from(0))
-  const [debtRatio, setDebtRatio] = useState<number>(0)
   const [holderLength, setHolderLength] = useState<number>(0)
-  const [initialSupply, setInitialSupply] = useState<number>(0)
-  const [inflation, setInflation] = useState<number>(0)
-  const [marketPrice, setMarketPrice] = useState<number>(0)
-  const [HouseHoldSavingsRate, setHouseHoldSavingsRate] = useState<string>('0')
-  const [DSGE, setDSGE] = useState<string>('0')
-  const [addLiquiditySupply, setAddLiquiditySupply] = useState<BigNumber>(BigNumber.from(0))
-  const [bondRewardWithdrawed, setBondRewardWithdrawed] = useState<BigNumber>(BigNumber.from(0))
-  const [costSupply, setCostSupply] = useState<BigNumber>(BigNumber.from(0))
 
   const clickTab = (tab: string) => {
     setActiveTab(tab)
@@ -91,113 +75,97 @@ const Dashboard = () => {
   const dfs = useDFSContract()
   const bond = useBondContract()
   const { data } = useSWR('dashboard', async () => {
-    const callFactor = await dfsMining.totalCalls()
-    setCallfactor(callFactor.toNumber())
-
-    const initialSupply = await dfs.initialSupply()
-    setInitialSupply(initialSupply)
-
-    setDSGE(await dfsMining.DSGE())
-
-    setHouseHoldSavingsRate(await dfsMining.HouseHoldSavingsRate())
+    const dashboard = {
+      callFactor: await dfsMining.totalCalls(),
+      initialSupply: await dfs.initialSupply(),
+      DSGE: await dfsMining.DSGE(),
+      houseHoldSavingsRate: await dfsMining.HouseHoldSavingsRate(),
+      tvl: BigNumber.from(0),
+      marketPrice: parseFloat(formatUnits(await bond.getPriceInUSDT())),
+      foundationDFS: await dfs.balanceOf(foundation),
+      elementaryPayoutMintAddressDfs: await dfs.balanceOf(elementaryPayoutMintAddress),
+      advancedPayoutMintAddressDfs: await dfs.balanceOf(advancedPayoutMintAddress),
+      elementaryMintAddressDfs: await dfs.balanceOf(elementaryMintAddress),
+      advancedMintAddressDfs: await dfs.balanceOf(advancedMintAddress),
+      daoDFS: BigNumber.from(0),
+      bondDfs: await dfs.balanceOf(bond.address),
+      addLiquiditySupply: await bond.addLiquiditySupply(),
+      customSupply: await bond.customSupply(),
+      dfsTotalSupply: await dfs.totalSupply(),
+      costSupply: await bond.costSupply(),
+      totalPayout: await bond.totalPayout(),
+      unstakeNFTDFS: await dfs.balanceOf(unstakeNFTAddress),
+      nftMarketDestroyedDFS: await dfs.balanceOf(nftMarketDestroyAddress),
+      withdrawedSocialReward: await dfsMining.withdrawedSocialReward(),
+      withdrawedSavingReward: await dfsMining.withdrawedSavingReward(),
+      bondUsed: BigNumber.from(0),
+      withdrawed: BigNumber.from(0),
+      solitaryReserves: 0,
+      inflationRate: 0,
+      debtRatio: 0,
+      currentCirculationSupply: BigNumber.from(0),
+      totalCirculationSupply: BigNumber.from(0),
+      targetInflationRate: '0',
+    }
 
     const reserves = await pair.getReserves()
     const [numerator, denominator] =
       usdtAddress.toLowerCase() < dfsAddress.toLowerCase() ? [reserves[0], reserves[1]] : [reserves[1], reserves[0]]
-    setTvl(numerator.mul(2))
+    dashboard.tvl = numerator.mul(2)
 
-    const marketPrice = parseFloat(formatUnits(await bond.getPriceInUSDT()))
-
-    setMarketPrice(marketPrice)
-
-    const foundationDFS = await dfs.balanceOf(foundation)
-    setFoundationDFS(foundationDFS)
-
-    const totalPayout = await bond.totalPayout()
-
-    const unstakeNFTDFS = await dfs.balanceOf(unstakeNFTAddress)
-
-    const nftMarketDestroyedDFS = await dfs.balanceOf(nftMarketDestroyAddress)
-
-    const elementaryPayoutMintAddressDfs = await dfs.balanceOf(elementaryPayoutMintAddress)
-    const advancedPayoutMintAddressDfs = await dfs.balanceOf(advancedPayoutMintAddress)
-    const elementaryMintAddressDfs = await dfs.balanceOf(elementaryMintAddress)
-    const advancedMintAddressDfs = await dfs.balanceOf(advancedMintAddress)
-    const daoDFS = (await Promise.all(dao.map(async (d) => dfs.balanceOf(d)))).reduce((accum, curr) => {
+    dashboard.daoDFS = (await Promise.all(dao.map(async (d) => dfs.balanceOf(d)))).reduce((accum, curr) => {
       // eslint-disable-next-line no-return-assign, no-param-reassign
       accum = accum.add(curr)
       return accum
     }, BigNumber.from(0))
 
     setHolderLength(await dfs.getHoldersLength())
-    const bondDfs = await dfs.balanceOf(bond.address)
-
-    const addLiquiditySupply = await bond.addLiquiditySupply()
-    setAddLiquiditySupply(addLiquiditySupply)
-    // console.log(formatUnits(addLiquiditySupply))
-    const customSupply = await bond.customSupply()
-    const dfsTotalSupply = await dfs.totalSupply()
-    const costSupply = await bond.costSupply()
-    setCostSupply(costSupply)
-    const currentCirculationSupply = dfsTotalSupply
-      .sub(daoDFS)
-      .sub(foundationDFS)
-      .sub(bondDfs)
-      .sub(unstakeNFTDFS)
-      .sub(nftMarketDestroyedDFS)
-      .sub(elementaryPayoutMintAddressDfs)
-      .sub(advancedPayoutMintAddressDfs)
-      .sub(elementaryMintAddressDfs)
-      .sub(advancedMintAddressDfs)
-      .sub(initialSupply)
-      .add(addLiquiditySupply)
-      .add(costSupply)
-
-    setCirculationSupply(currentCirculationSupply)
-
-    if (circulationSupply.gt(0)) {
-      const solitaryReserves = parseFloat(formatUnits(numerator)) / parseFloat(formatUnits(circulationSupply))
-      setSingleCurrencyReserves(solitaryReserves)
-
-      if (marketPrice > 0 && solitaryReserves > 0) {
-        setInflation((marketPrice - solitaryReserves) / marketPrice)
-      }
-    }
-
-    const withdrawedSocialReward = await dfsMining.withdrawedSocialReward()
-    const withdrawedSavingReward = await dfsMining.withdrawedSavingReward()
 
     const buyers = await bond.getBuyers()
 
-    const bondStatistic = { bondUsed: BigNumber.from(0), withdrawed: BigNumber.from(0) }
     await Promise.all(
       buyers.map(async (buyer) => {
-        const referralBond = await bond.addressToReferral(buyer)
-        bondStatistic.bondUsed = bondStatistic.bondUsed.add(referralBond.bondUsed)
-        bondStatistic.withdrawed = bondStatistic.withdrawed.add(referralBond.bondRewardWithdrawed)
+        const referral = await bond.addressToReferral(buyer)
+        dashboard.bondUsed = dashboard.bondUsed.add(referral.bondUsed)
+        dashboard.withdrawed = dashboard.withdrawed.add(referral.bondRewardWithdrawed)
       }),
     )
-    setBondRewardWithdrawed(bondStatistic.withdrawed)
-    const totalCirculation = totalPayout
+    dashboard.currentCirculationSupply = dashboard.dfsTotalSupply
+      .sub(dashboard.daoDFS)
+      .sub(dashboard.foundationDFS)
+      .sub(dashboard.bondDfs)
+      .sub(dashboard.unstakeNFTDFS)
+      .sub(dashboard.nftMarketDestroyedDFS)
+      .sub(dashboard.elementaryPayoutMintAddressDfs)
+      .sub(dashboard.advancedPayoutMintAddressDfs)
+      .sub(dashboard.elementaryMintAddressDfs)
+      .sub(dashboard.advancedMintAddressDfs)
+      .sub(dashboard.initialSupply)
+      .add(dashboard.addLiquiditySupply)
+      .add(dashboard.costSupply)
+
+    dashboard.totalCirculationSupply = dashboard.totalPayout
       .mul(1250)
       .div(1000)
-      .add(withdrawedSocialReward)
-      .add(withdrawedSavingReward)
-      .add(bondStatistic.withdrawed)
-      .add(addLiquiditySupply)
-      .add(customSupply)
-      .add(costSupply)
+      .add(dashboard.withdrawedSocialReward)
+      .add(dashboard.withdrawedSavingReward)
+      .add(dashboard.withdrawed)
+      .add(dashboard.addLiquiditySupply)
+      .add(dashboard.customSupply)
+      .add(dashboard.costSupply)
 
-    setTotalCirculation(totalCirculation)
-
-    setTotalBondUsed(bondStatistic.bondUsed)
-
-    if (circulationSupply.gt(0)) {
+    if (dashboard.currentCirculationSupply.gt(0)) {
+      dashboard.solitaryReserves =
+        parseFloat(formatUnits(numerator)) / parseFloat(formatUnits(dashboard.currentCirculationSupply))
       const debtRatio =
-        (parseFloat(formatUnits(totalPayout.sub(bondStatistic.bondUsed))) * 100) /
-        parseFloat(formatUnits(circulationSupply))
-      setDebtRatio(debtRatio)
+        (parseFloat(formatUnits(dashboard.totalPayout.sub(dashboard.bondUsed))) * 100) /
+        parseFloat(formatUnits(dashboard.currentCirculationSupply))
+      dashboard.debtRatio = debtRatio
+      if (dashboard.marketPrice > 0 && dashboard.solitaryReserves > 0) {
+        dashboard.inflationRate = (dashboard.marketPrice - dashboard.solitaryReserves) / dashboard.marketPrice
+      }
     }
+
     // console.log("totalCirculation:",formatUnits(totalCirculation))
 
     // const telegram = await fetch(telegramLink)
@@ -235,8 +203,12 @@ const Dashboard = () => {
 
     const response = await fetch('https://middle.diffusiondao.org/dashboard')
     const json = await response.json()
-    return json
+    console.log(dashboard)
+    return { ...dashboard, ...json }
   })
+
+  console.log('data:', data)
+
   const conentractions = Object.keys(data?.concentration ?? {}).map((key) => data?.concentration[key])
   // eslint-disable-next-line no-return-assign, no-param-reassign
   const avgConentraction = conentractions.reduce((acc, cur) => (acc += cur), 0) / conentractions.length
@@ -248,6 +220,10 @@ const Dashboard = () => {
   const usdtAddress = getUSDTAddress()
   const dfsAddress = getDFSAddress()
 
+  const expansionFund =
+    data?.foundationDFS && data?.marketPrice
+      ? (parseFloat(formatUnits(data?.foundationDFS)) * data?.marketPrice).toFixed(2)
+      : 0
   return (
     <div className="dashboard-view">
       <Typography variant="h4" style={{ fontWeight: 700, overflow: 'hidden', color: '#fff' }}>
@@ -287,7 +263,7 @@ const Dashboard = () => {
                           <div className="cell-sub-item">
                             <DataCell
                               title={t('TVL')}
-                              data={`$${formatBigNumber(tvl, 2)}`}
+                              data={`$${formatBigNumber(data?.tvl ?? BigNumber.from(0), 2)}`}
                               style={{ fontSize: '32px' }}
                             />
                             <DataCell title="" data="" imgUrl="/images/dashboard/tvl.svg" />
@@ -297,21 +273,24 @@ const Dashboard = () => {
                           <div className={`${classes.hasRLBorder} cell-sub-item`}>
                             <DataCell
                               title={t('Total circulation')}
-                              data={`${formatBigNumber(totalCirculation, 2)} DFS`}
+                              data={`${formatBigNumber(data?.totalCirculationSupply ?? BigNumber.from(0), 2)} DFS`}
                             />
-                            <DataCell title={t('Solitary Reserve')} data={`$${formatNumber(solitaryReserves, 2)}`} />
+                            <DataCell
+                              title={t('Solitary Reserve')}
+                              data={`$${formatNumber(data?.solitaryReserves ?? 0, 2)}`}
+                            />
                           </div>
                         </Grid>
                         <Grid item lg={4} md={4} sm={12} xs={12}>
                           <div className="cell-sub-item">
                             <DataCell
                               title={t('Current circulation volume')}
-                              data={`${formatBigNumber(circulationSupply, 2)} DFS`}
+                              data={`${formatBigNumber(data?.currentCirculationSupply ?? BigNumber.from(0), 2)} DFS`}
                               imgUrl="/images/dashboard/rf.svg"
                             />
                             <DataCell
                               title={t('Expansion Fund')}
-                              data={`$${(parseFloat(formatUnits(foundationDFS)) * marketPrice).toFixed(2)}`}
+                              data={`$${expansionFund}`}
                               imgUrl="/images/dashboard/rm.svg"
                             />
                           </div>
@@ -344,7 +323,7 @@ const Dashboard = () => {
                             }}
                             value={8}
                           />
-                          <div className="ctir-data">{8}%</div>
+                          <div className="ctir-data">{data?.targetInflationRate}%</div>
                         </div>
                         <div className="ctir-title">{t('Target inflation rate')}</div>
                       </div>
@@ -360,7 +339,7 @@ const Dashboard = () => {
                           >
                             <DataCell
                               title={t('Household savings rate')}
-                              data={`${HouseHoldSavingsRate}%`}
+                              data={`${data?.houseHoldSavingsRate ?? 0}%`}
                               progressColor="#f200ff"
                             />
                           </div>
@@ -370,14 +349,18 @@ const Dashboard = () => {
                             className="cell-sub-item"
                             style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.05)' }}
                           >
-                            <DataCell title={t('DSGE suitability')} data={`${DSGE}%`} progressColor="#01ffed" />
+                            <DataCell
+                              title={t('DSGE suitability')}
+                              data={`${data?.DSGE ?? 0}%`}
+                              progressColor="#01ffed"
+                            />
                           </div>
                         </Grid>
                         <Grid item lg={6} md={6} sm={12} xs={12}>
                           <div className="cell-sub-item">
                             <DataCell
                               title={t('Inflation')}
-                              data={`${(inflation * 100).toFixed(2)}%`}
+                              data={`${(data?.inflationRate ?? 0 * 100).toFixed(2)}%`}
                               progressColor="#f5d700"
                             />
                           </div>
@@ -386,7 +369,7 @@ const Dashboard = () => {
                           <div className="cell-sub-item">
                             <DataCell
                               title={t('Debt ratio')}
-                              data={`${formatNumber(debtRatio, 2)}%`}
+                              data={`${formatNumber(data?.debtRatio ?? 0, 2)}%`}
                               progressColor="#0131ff"
                             />
                           </div>
@@ -424,7 +407,7 @@ const Dashboard = () => {
                         />
                         <DataCell
                           title={t('Call Factor')}
-                          data={callFactor.toString()}
+                          data={data?.callFactor.toString()}
                           imgUrl="/images/dashboard/cf.png"
                           titleStyle={{ color: '#ABB6FF' }}
                           imgStyle={{ height: '85px', width: '54px' }}
@@ -437,7 +420,7 @@ const Dashboard = () => {
                       <div className="cell-sub-item">
                         <DataCell
                           title={t('Expansion Fund')}
-                          data={`$${(parseFloat(formatUnits(foundationDFS)) * marketPrice).toFixed(2)}`}
+                          data={`$${expansionFund}`}
                           imgUrl="/images/dashboard/rz.svg"
                         />
                       </div>
